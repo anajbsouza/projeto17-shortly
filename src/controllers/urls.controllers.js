640b44d2c0aad094e1de607ff1668647017b2e83
anajbsouza <anaju.batsouza@gmail.com>
2023-08-07 01:02:56 -0300

    feat: dump do banco

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -45,3 +45,21 @@ export async function redirectToUrl(req, res) {
     }
 }
 
+export async function deleteUrl(req, res) {
+
+    try {
+        const urlId = req.params.id;
+        const userId = res.locals.session.user_id;
+        
+        const urlResult = await db.query("SELECT * FROM short_urls WHERE id = $1", [urlId]);
+        const urlRecord = urlResult.rows[0];
+
+        if (!urlRecord) return res.sendStatus(404); 
+        if (urlRecord.user_id !== userId) return res.sendStatus(401); 
+        await db.query("DELETE FROM short_urls WHERE id = $1", [urlId]);
+
+        res.sendStatus(204);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { userValidation } from "../middlewares/userValidation.middleware.js";
-import { shortenUrl, getUrlById, redirectToUrl } from "../controllers/urls.controllers.js";
+import { shortenUrl, getUrlById, redirectToUrl, deleteUrl } from "../controllers/urls.controllers.js";
 
 const urlRouter = Router();
 
 urlRouter.post("/urls/shorten", userValidation, shortenUrl);
 urlRouter.get("/urls/:id", getUrlById);
 urlRouter.get("/urls/open/:shortUrl", redirectToUrl);
+urlRouter.delete("/urls/:id", deleteUrl);
 
 export default urlRouter;
